557c351acc388b21969ef468ac5ba20ad105d56a
Muhammad Osman <139002050+MuhammadOsman381@users.noreply.github.com>
2024-02-16 19:50:29 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// let ham = document.querySelector('#hamb');
+// var list = document.querySelector('#list');
+
+
+// ham.addEventListener('click',function(){
+
+//     if(ham.classList.contains('fa-bars'))
+//     {
+//         ham.classList.add("fa-xmark");
+//         list.style.display = "grid";
+//     }
+    
+// })
